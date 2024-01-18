d2b22796d61c64397115f01260e3bfe5ac798a8b
Valdo <vteixeiraromao@gmail.com>
2024-01-18 16:45:18 +0000

    Add auditorium and exterior

--- src/main.ts
+++ src/main.ts
@@ -4,20 +4,19 @@ import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
 console.log('Script started successfully');
 
-let currentPopup: any = undefined;
-
 // Waiting for the API to be ready
 WA.onInit().then(() => {
     console.log('Scripting API ready');
     console.log('Player tags: ',WA.player.tags)
 
-    WA.room.area.onEnter('clock').subscribe(() => {
-        const today = new Date();
-        const time = today.getHours() + ":" + today.getMinutes();
-        currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
-    })
-
-    WA.room.area.onLeave('clock').subscribe(closePopup)
+    WA.room.area.onEnter("roofZoneAuditorium").subscribe(() => {
+        WA.room.hideLayer("Roof/roofAuditorium");
+        WA.room.hideLayer("Roof/roofAuditorium2");
+    });
+    WA.room.area.onLeave("roofZoneAuditorium").subscribe(() => {
+        WA.room.showLayer("Roof/roofAuditorium");
+        WA.room.showLayer("Roof/roofAuditorium2");
+    });
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
@@ -25,12 +24,5 @@ WA.onInit().then(() => {
     }).catch(e => console.error(e));
 
 }).catch(e => console.error(e));
-
-function closePopup(){
-    if (currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
-    }
-}
 
 export {};
